9f2d934695966870fda173df2a35089e23765525
SYSTEM <SYSTEM@mtsup-AA-1.us.accela.com>
2022-05-11 16:19:09 -0500

    commit by emse tools 2022-05-11 16:19:09

---
+++ Scripts/CONF_ENVHEALTH_LANDUSE_RECORD_VALIDATION.js
@@ -0,0 +1,80 @@
+{
+    "EnvHealth/Land Use/*/Application": {
+      "WorkflowTaskUpdateBefore": [
+        {
+          "metadata": {
+            "description": "Rule for all EnvHealth Land Use issuance",
+            "operator": ""
+          },
+          "preScript": "",
+          "criteria": {
+            "disabled": true,
+            "task": [
+              "Permit Issuance" ,
+              "Decision Notification"
+            ],
+            "status": [
+              "Issued" ,
+              "Modification Request Approved"
+            ],
+            "allowBalance": false
+          },
+          "action": {
+            "validationMessage": "This action cannot be taken until all outstanding fees are paid in full."
+          },
+          "postScript": ""
+        }
+        ,
+        {
+            "metadata": {
+              "description": "Rule for all EnvHealth issuance",
+              "operator": ""
+            },
+            "preScript": "",
+            "criteria": {
+                "disabled": true,
+              "task": [
+                  "Permit Issuance" ,
+                  "Decision Notification"
+              ],
+              "status": [
+                 "Issued" ,
+                 "Modification Request Approved"
+              ],
+              "requiredContact": [
+                "Facility Owner"
+              ]
+            },
+            "action": {
+              "validationMessage": "This action cannot be taken until adding contact Facility Owner."
+            },
+            "postScript": ""
+          },
+          {
+              "metadata": {
+                "description": "Rule for all EnvHealth issuance",
+                "operator": ""
+              },
+              "preScript": "",
+              "criteria": {
+                "disabled": true,
+                "task": [
+                      "Permit Issuance" ,
+                      "Decision Notification"
+                ],
+                "status": [
+                           "Issued" ,
+                           "Modification Request Approved"
+                ],
+                "requiredContact": [
+                  "Accounts Receivable"
+                ]
+              },
+              "action": {
+                "validationMessage": "This action cannot be taken until adding contact Accounts Receivable."
+              },
+              "postScript": ""
+            }
+      ]
+    }
+  }
